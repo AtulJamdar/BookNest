fb67430eb1a3a6ebf7a1c4db3a846940c299fbb5
AtulJamdar <work.atuljamdar@gmail.com>
2026-03-03 17:23:25 +0530

    acuth middleware added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ debug: false });
 
 const connectDB = async() => {
     try {
         const conn = await mongoose.connect(process.env.MonGODB_URI);
         console.log("MongoDB Connected");
-
+        // return conn;
     } catch (error) {
         console.log(`Error:${error.message}`);
         process.exit(1);
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
-require('dotenv').config();
+require('dotenv').config({ debug: false });
+const cors = require('cors');
 
 const app = express();
 
+//Middleware
+app.use(cors());
 app.use(express.json());
+
+//Connect Database
 connectDB();
 
 app.get('/', (req, res) => {
@@ -14,4 +19,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
