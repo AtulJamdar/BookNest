cc4b4d804ff032f38533cbdfc2833ac78b5fc2a7
AtulJamdar <work.atuljamdar@gmail.com>
2026-03-03 16:20:14 +0530

    Issue Schema Added

---
+++ backend/models/Issue.js
@@ -0,0 +1,33 @@
+const mongoose = required('mongoose')
+
+const issueSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    bookId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true,
+    },
+    issueDate: {
+        type: Date,
+        default: Date.now,
+    },
+    dueDate: {
+        type: Date,
+        required: true,
+    },
+    returnedDate: {
+        type: Date,
+        default: null,
+    },
+    status: {
+        type: String,
+        enum: ['issued', 'returned'],
+        default: 'issued',
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Issue', issueSchema);
