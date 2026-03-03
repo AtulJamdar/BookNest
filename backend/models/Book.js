53deef79515f028c73a1820fa8a89cb370bb3f47
AtulJamdar <work.atuljamdar@gmail.com>
2026-03-03 16:09:01 +0530

    Book Model added

---
+++ backend/models/Book.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    isbn: {
+        type: String,
+        unique: true,
+        sparse: true,
+    },
+    totalCopies: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    availableCopies: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Book', bookSchema);
